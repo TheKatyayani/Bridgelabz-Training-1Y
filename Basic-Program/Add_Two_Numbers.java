b6eb45251538ee5aa41dc69bc870286c5a8a1500
TheKatyayani <koyal2102@gamil.com>
2026-01-10 12:17:29 +0530

    Basic-Program

---
+++ Basic-Program/Add_Two_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Add_Two_Numbers{
+      public static void main(String[] args){
+	    Scanner sc = new Scanner(System.in);
+		
+		int num_1 = sc.nextInt();
+		int num_2 = sc.nextInt();
+		
+		int add = num_1 + num_2;
+		
+		System.out.println("sum is "+ add);
+		}
+		}
