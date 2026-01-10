63e072ed56fd23655bfd150f673f7248640f338a
TheKatyayani <koyal2102@gamil.com>
2026-01-10 14:10:04 +0530

    Basic-Program

--- Basic-Program/Perimeter_of_a_Rectangle.java
+++ Basic-Program/Perimeter_of_a_Rectangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Volume_of_a_Cylinder{
+public class Perimeter_of_a_Rectangle{
       public static void main(String[] args){
 	    Scanner sc = new Scanner(System.in);
 		
