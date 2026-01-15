cafb70f7161ac512f9ee7e6fcab0ae389881936d
TheKatyayani <koyal2102@gamil.com>
2026-01-15 06:47:01 +0530

    Basic-Programs

---
+++ Basic-Programs/Profit_Loss.java
@@ -0,0 +1,14 @@
+public class Profit_Loss {
+        public static void main(String[] args){
+		       
+			   double selling_price = 191;
+			   double cost_price = 129;
+			   double profit = selling_price - cost_price;
+			   
+			   double profit_percent = (profit / cost_price) * 100 ;
+			   
+			   System.out.println("The Cost Price is INR " + cost_price + " and Selling Price is INR " + selling_price + "\nThe Profit is INR "+ profit + " and the Profit Percentage is " + profit_percent);
+			   
+		}
+		
+	}
