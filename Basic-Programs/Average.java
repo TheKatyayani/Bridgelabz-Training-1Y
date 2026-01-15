063c8ec1f637a6a23ecd862b72c93b71ba5d893b
TheKatyayani <koyal2102@gamil.com>
2026-01-15 06:06:27 +0530

    Basic-Programs

---
+++ Basic-Programs/Average.java
@@ -0,0 +1,12 @@
+public class Average{
+        public static void main(String[] argas){
+		        
+				int mark_in_math = 94;
+				int mark_in_physics = 95;
+				int mark_in_chemistry = 96;
+				
+				double average_marks = (mark_in_math + mark_in_physics + mark_in_chemistry)/3;
+				
+				System.out.println("Sam's average mark in PCM is " + average_marks);
+			}
+		}
