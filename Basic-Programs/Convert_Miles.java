b8515928a56cd11b79e7f21a37db63cf9e20e8f6
TheKatyayani <koyal2102@gamil.com>
2026-01-15 06:19:48 +0530

    Basic-Programs

---
+++ Basic-Programs/Convert_Miles.java
@@ -0,0 +1,10 @@
+public class Convert_Miles {
+       public static void main(String [] args){
+	          
+			  double distance = 10.8;
+			  double distance_in_miles = (distance * (1.6) );
+			  
+			  System.out.println("The distance "+ distance + " in miles "+ distance_in_miles);
+			  
+		}
+	}
