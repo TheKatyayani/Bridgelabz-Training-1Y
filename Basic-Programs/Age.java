6b99c2ec7228b9843b5d88988563fb1c22253e57
TheKatyayani <koyal2102@gamil.com>
2026-01-15 05:51:02 +0530

    Basic-Programs

---
+++ Basic-Programs/Age.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Age { 
+      public static void main(String[] args){
+	        
+			int current_year = 2024;
+			int birth_year = 2020;
+			
+			int age = current_year - birth_year;
+			
+			System.out.println("Harry's age in "+ current_year +" is "+ age);
+			
+		}
+	}
