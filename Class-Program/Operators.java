e8dc7342d899970f985c054999377e248d885614
TheKatyayani <koyal2102@gamil.com>
2026-01-22 15:21:26 +0530

    [Kirti koyal]  operators and loops

---
+++ Class-Program/Operators.java
@@ -0,0 +1,46 @@
+public class Operators{
+       public static void main(String[] args){
+	   
+	   int i = 0;
+	   System.out.println("value " + i++);  //0
+	   i++;
+	   System.out.println("value " + i);    //1
+	   ++i;
+	   System.out.println("value " + i++);  //3
+	   System.out.println("value " + i);    //4
+	   
+	   
+	   
+	   //LOOPS
+	   
+	   
+	   // for 
+	   
+	for(int  z=0;z<10;z++){
+	System.out.println("i: " + z);
+	
+	}  
+
+     //while
+      
+	  int j = 10;
+	   while(j>0){
+	   System.out.println("j: " + j);
+	   j--;
+	   }
+	   
+	   //for
+	   
+	   int k=10;
+	   do{
+	   System.out.println("k: " + k );
+	   k--;
+	   }while(k>0);
+	   
+	   //do-while , it will atleast run first...
+
+ 
+
+    }
+
+}
