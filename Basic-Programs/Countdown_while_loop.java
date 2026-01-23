59cb110d45338dd479f1a1454ed1796dbfeb302c
TheKatyayani <koyal2102@gamil.com>
2026-01-23 06:18:48 +0530

    [Kirti koyal]  Assignment-3

---
+++ Basic-Programs/Countdown_while_loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Countdown_while_loop{
+       public static void main(String [] args){
+	   
+	   Scanner sc = new Scanner(System.in);
+	   
+	   int n1 = sc.nextInt();
+	  
+	  while(n1>=1){
+	  System.out.println(n1);
+	  n1--;
+	   
+	   }
+}
+}
