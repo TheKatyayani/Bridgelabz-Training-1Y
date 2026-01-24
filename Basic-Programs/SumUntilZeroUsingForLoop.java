201b26ecb9f93659d912478bc079e0042930d309
TheKatyayani <koyal2102@gamil.com>
2026-01-24 17:13:38 +0530

    [kirti koyal] Assignment 3[1]

---
+++ Basic-Programs/SumUntilZeroUsingForLoop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumUntilZeroUsingForLoop{
+       public static void main(String [] args){
+	   
+	   Scanner sc = new Scanner(System.in);
+	   
+	   
+	   double n1=sc.nextDouble();;
+	   double sum =0;
+	  
+	 
+	   for(double i= n1;i !=0;){   
+		  sum = sum + i;
+		  
+		  System.out.println("Enter number again for the sum : ");
+	     
+ 		  i = sc.nextDouble();
+	
+	   
+	  }
+	  
+	  System.out.println("Total sum untill zero is " + sum);
+}
+}
