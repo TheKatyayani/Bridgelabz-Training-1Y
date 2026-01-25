45a213e4259c9e5e4063aa45b90601d6fb579b3c
TheKatyayani <koyal2102@gamil.com>
2026-01-25 19:29:59 +0530

    [kirti koyal] Assignment 3[2].

--- Basic-Programs/FactorsWithWhile.java
+++ Basic-Programs/FactorsWithWhile.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
-public class Factors{
+public class FactorsWithWhile{
        public static void main(String [] args){
 	   
 	   Scanner sc = new Scanner(System.in);
 	   
 	   int n1 = sc.nextInt();
+	   int i=1;
 	    
 		if(n1>0){
-			for(int i=1;i<n1+1;i++){
+			
+			while(i<=n1){
 				 if(n1%i==0){
 					 System.out.println("Factors of number "+  n1 + " contain " + i);
-				 }
+					 }
+					 i++;
 			}
 		}else{
 			System.out.println("YOU given number is not positive integer.");
