75945b41854968f564f5693f0abde46d13520841
TheKatyayani <koyal2102@gamil.com>
2026-01-26 09:38:04 +0530

    [kirti koyal] Assignment 3[2]

--- Basic-Programs/FizzBuzzWhileLoop.java
+++ Basic-Programs/FizzBuzzWhileLoop.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class FizzBuzz{
+public class FizzBuzzWhileLoop{
        public static void main(String [] args){
 	   
 	        Scanner sc = new Scanner(System.in);
 
 	        int n1 = sc.nextInt();
-
+            int i = 1;
 			  
 			    if(n1>0){
 			     
-			       for(int i=1;i<=n1;i++){
+			       while(i<=n1){
 					   if(n1%i==0){
 					   
 					      if(n1%3==0 && n1%5==0){
@@ -23,6 +23,7 @@ public class FizzBuzz{
 						    System.out.println("Multiple of "+ n1 + " contain = " + i );
 					      }
 				        }
+						i++;
 				    }
 			        
 				}else{
