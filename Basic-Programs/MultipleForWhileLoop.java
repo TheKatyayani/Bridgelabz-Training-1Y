64d8f563db6ad113f03d72fd56f7e9ab2e837a07
TheKatyayani <koyal2102@gamil.com>
2026-01-26 08:08:11 +0530

    [kirti koyal] Assignment 3[1]

--- Basic-Programs/MultipleForWhileLoop.java
+++ Basic-Programs/MultipleForWhileLoop.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class MultipleForLoop{
+public class MultipleForWhileLoop{
        public static void main(String [] args){
 	   
 	   Scanner sc = new Scanner(System.in);
 	   
 	   int n1 = sc.nextInt();
-	   
+	   int i=100;
 	    
 		if(n1>0){
-			for(int i=100;i>=1;i--){
+			while(i>=1){
 				 if(i%n1==0){
 				 System.out.println("Multiple of "+  n1 + " , conatin " + i);   
 			     
 				 }
-			
+			    i--;
 			}
 		}else{
 			System.out.println("Your given number is not positive integer.");
