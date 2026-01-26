84c9f25008a486bea87858e536c609adb7978b14
TheKatyayani <koyal2102@gamil.com>
2026-01-26 09:09:44 +0530

    [kirti koyal] Assignment 3[2]

--- Basic-Programs/PowerForLoop.java
+++ Basic-Programs/PowerForLoop.java
@@ -7,19 +7,19 @@ public class PowerForLoop{
 	   
 	   System.out.println("Enter [1].power and [2].Number.");
 	   int power = sc.nextInt();
-	   int n = sc.nextInt();
+	   int num = sc.nextInt();
 	   int result = 1;
 	    
-		if(n>0 && power>0){
+		if(num>0 && power>0){
 			for(int i = 1 ; i<=power;i++){
-                result = result*n;
+                result = result*num;
 		
 			}
 		}else{
 			System.out.println("Your given number is not positive integer.");
 		}
 		
-		System.out.println("The number "+ n + " with power "+ power + " result is " + result);
+		System.out.println("The number "+ num + " with power "+ power + " result is " + result);
 		
 		
 	   }
