0b20df4c1b88a7e53c4f5681b6a9f730ef795d74
TheKatyayani <koyal2102@gamil.com>
2026-02-04 23:41:46 +0530

    [kirti koyal] Assignment 4[2]

---
+++ ReverseArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ReverseArray {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter lenght of number");
+        int n = sc.nextInt();
+        System.out.println("Enter number");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+    }
+
+}
