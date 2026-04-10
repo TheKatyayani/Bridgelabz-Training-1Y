7e8462c061a09e7af207207f4724cb58a28409c8
TheKatyayani <koyal2102@gamil.com>
2026-04-10 11:33:24 +0530

    Test review

---
+++ src/com/gla/Test/src/RootTracker/Driverdetails.java
@@ -0,0 +1,23 @@
+package RootTracker;
+
+public class Driverdetails {
+
+   private String driverId;
+   private String name;
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
